19bebaea5fbfa62a6d6dadcb48855bfdb3405d3a
rj153 <rj153@duke.edu>
2024-08-22 02:17:18 -0400

    django migrate

--- blog/models.py
+++ blog/models.py
@@ -29,7 +29,7 @@ class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
 
-    author = models.ManyToManyField(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
