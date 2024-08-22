7dc9b750b996aa6f1058ab76cefe2146a215bd5f
rj153 <rj153@duke.edu>
2024-08-22 02:02:57 -0400

    create blog models, category, tag & post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# each post belongs to a category, and each category can have multiple posts
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+# each post can have multiple tags, and each tag can have multiple posts
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+# post: title, body, created_time, modified_time, excerpt (allowed to be empty), category, tags, author
+class Post(models.Model):
+    title = models.CharField(max_length=70)
+    body = models.TextField()
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    excerpt = models.CharField(max_length=200, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    author = models.ManyToManyField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
