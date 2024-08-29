50bcdae8698a4a9e194e825aa67a9679c0360afa
rj153 <rj153@duke.edu>
2024-08-29 01:42:01 -0400

    submit & display comments functionality

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_POST
+from django.contrib import messages
+from .forms import CommentForm
+from blog.models import Post
 
 # Create your views here.
+
+@require_POST
+def comment(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+
+    form = CommentForm(request.POST)
+
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+        messages.add_message(request, messages.SUCCESS, 'Comment submitted successfully!', extra_tags='success')
+        return redirect(post)
+
+    context = {
+        'post': post,
+        'form': form,
+    }
+    messages.add_message(request, messages.ERROR, 'Comment submitted failed!', extra_tags='danger')
+    return render(request, 'comments/preview.html', context=context)
