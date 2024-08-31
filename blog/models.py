a7a4e9728fd11ef0f7dcd496caa495e5e0e28590
rj153 <rj153@duke.edu>
2024-08-31 17:53:42 -0400

    read count

--- blog/models.py
+++ blog/models.py
@@ -31,6 +31,7 @@ class Post(models.Model):
     excerpt = models.CharField(max_length=200, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
+    views = models.PositiveIntegerField(default = 0, editable = False)
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -52,3 +53,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
+    
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
